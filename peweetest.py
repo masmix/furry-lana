d1c9b70491eef3987d8c06efbabbb175e62bd976
masmix <wkn@wp.pl>
2014-06-04 09:18:15 +0200

    some changes in pewtest.py

--- peweetest.py
+++ peweetest.py
@@ -4,13 +4,6 @@ from peewee import *
 
 db = MySQLDatabase('baza_test_python', user='bazek',passwd='ve7128')
 
-#class Book(peewee.Model):
-#    author = peewee.CharField()
-#    title = peewee.TextField()
-
- #   class Meta:
-  #      database = db
-
 class Info(peewee.Model):
     data = DateField(db_column='Data')
     godz = CharField(max_length=10, db_column='Godz')
@@ -26,6 +19,7 @@ class Info(peewee.Model):
     class Meta:
         database = db
 
+#Info.create_table()
 fields = find_sections.returnDictFromFile()
 #fields={'godz': ' 9:05:00', 'konto': '', 'program': 'SMALL BUSINESS 5.1.3021.4980', 'nazwa': 'RZEPKA Pruszowice ul.Sloneczna 13          tel.071 31 54 603, e-mail: wkn@wp.pl', 'nip': '911-167-70-58', 'data': '14.04.24', 'kod': '51-217', 'kodowanie': 'Windows 1250', 'miasto': 'Pruszowice'}
 
